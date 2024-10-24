9bca1e91a860db8942bb844bc3221c800d627fb7
Aneliya21 <anelka.k.03@gmail.com>
2024-10-24 10:00:18 +0300

    updated lab01

--- Labs/lab01/CourseScheduler.java
+++ Labs/lab01/CourseScheduler.java
@@ -1,2 +1,80 @@
-tbd
+public class CourseScheduler {
+
+    public static void swapTwoIntervals(int[] a, int[] b) {
+        int temp = a[0];
+        a[0] = b[0];
+        b[0] = temp;
+
+        int temp2 = a[1];
+        a[1] = b[1];
+        b[1] = temp2;
+    }
+    public static int compareTwoIntervals(int[] a, int[] b) {
+        if(a[0] == b[0] && a[1] == b[1]){
+            return 0;
+        }
+        else if(a[0] == b[0] && a[1] < b[1]){
+            return -1;
+        }
+        else if(a[0] == b[0] && a[1] > b[1]){
+            return 1;
+        }
+
+        else if(a[0] < b[0]){
+            return -1;
+        }
+        return 1; // if a[0] > b[0]
+    }
+    public static void sortCourses(int[][] courses) {
+        int rows = courses.length;
+        for (int i = 0; i < rows - 1; i++) {
+            for(int j = 0; j < rows - i - 1; j++) {
+                if(compareTwoIntervals(courses[j], courses[j + 1]) == 1){
+                    swapTwoIntervals(courses[j], courses[j + 1]);
+                }
+            }
+        }
+    }
+    public static int maxNonOverlappingCourses(int[][] courses) {
+
+        int rows = courses.length;
+
+        if(rows == 0) {
+            return 0;
+        }
+
+        sortCourses(courses);
+
+        int maxCount = 1, currentCount = 1;
+
+        int columnIndex = 0;
+        int currentEnd = courses[0][columnIndex + 1];
+
+        for (int i = 0; i < rows - 1; i++) {
+            for (int j = i + 1; j < rows; j++) {
+                if(currentEnd <= courses[j][columnIndex] && courses[j - 1][columnIndex] < courses[j][columnIndex]) {
+                    currentCount++;
+                    currentEnd = courses[j][columnIndex + 1];
+                }
+            }
+
+            currentEnd = courses[i + 1][columnIndex + 1];
+
+            if(currentCount > maxCount) {
+                maxCount = currentCount;
+            }
+            currentCount = 1;
+        }
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] courses = {{15, 18}, {9,11}, {12, 14}};
+
+        System.out.println(maxNonOverlappingCourses(courses));
+
+    }
+}
+
   
--- Labs/lab01/TextJustifier.java
+++ Labs/lab01/TextJustifier.java
@@ -1,1 +1,116 @@
-tbd
+public class TextJustifier {
+
+    private static String leftJustify(String[] line, int wordCount, int maxWidth) {
+        StringBuilder leftJustifiedLine = new StringBuilder();
+
+        for (int i = 0; i < wordCount; i++) {
+
+            leftJustifiedLine.append(line[i]);
+
+            if (i < wordCount - 1) {
+                leftJustifiedLine.append(' ');
+            }
+        }
+
+        while (leftJustifiedLine.length() < maxWidth) {
+            leftJustifiedLine.append(' ');
+        }
+        return leftJustifiedLine.toString();
+    }
+
+    private static String justify(String[] line, int wordCount, int lineLength, int maxWidth) {
+        if (wordCount == 1) {
+            return leftJustify(line, wordCount, maxWidth);
+        }
+
+        int totalSpaces = maxWidth - lineLength;
+        int spacesBetweenWords = totalSpaces / (wordCount - 1);
+        int extraSpaces = totalSpaces % (wordCount - 1);
+
+        StringBuilder justifiedLine = new StringBuilder();
+
+        for (int i = 0; i < wordCount; i++) {
+            justifiedLine.append(line[i]);
+
+            if (i < wordCount - 1) {
+
+                for (int j = 0; j < spacesBetweenWords; j++) {
+                    justifiedLine.append(' ');
+                }
+
+                if (extraSpaces > 0) {
+                    justifiedLine.append(' ');
+                    extraSpaces--;
+                }
+            }
+        }
+
+        return justifiedLine.toString();
+    }
+
+    public static String[] justifyText(String[] words, int maxWidth) {
+
+        if(words.length == 0){
+            return words;
+        }
+
+        String[] result = new String[words.length];
+        int resultIndex = 0, wordIndex = 0;
+
+        String[] currentLine = new String[words.length];
+        int currentLineLength = 0;
+        int currentWordCount = 0;
+
+        while (wordIndex < words.length) {
+            String word = words[wordIndex];
+
+            if (currentLineLength + word.length() + currentWordCount > maxWidth) {
+
+                result[resultIndex++] = justify(currentLine, currentWordCount, currentLineLength, maxWidth);
+
+                currentLine = new String[words.length];
+                currentLineLength = 0;
+                currentWordCount = 0;
+            }
+
+            currentLine[currentWordCount++] = word;
+            currentLineLength += word.length();
+            wordIndex++;
+        }
+
+        result[resultIndex++] = leftJustify(currentLine, currentWordCount, maxWidth);
+
+        String[] finalResult = new String[resultIndex];
+        System.arraycopy(result, 0, finalResult, 0, resultIndex);
+        return finalResult;
+    }
+
+    public static void main(String[] args) {
+
+        String[] words1 = {"The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog."};
+        int maxWidth1 = 11;
+        String[] result1 = justifyText(words1, maxWidth1);
+        for (String line : result1) {
+            System.out.println("\"" + line + "\"");
+        }
+
+        System.out.print('\n');
+
+        String[] words2 = {"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer."};
+        int maxWidth2 = 20;
+        String[] result2 = justifyText(words2, maxWidth2);
+        for (String line : result2) {
+            System.out.println("\"" + line + "\"");
+        }
+
+        System.out.print('\n');
+
+        String[] words3 = {"Hello", "my", "friend!", "I", "am", "in", "London."};
+        int maxWidth3 = 10;
+        String[] result3 = justifyText(words3, maxWidth3);
+        for (String line : result3) {
+            System.out.println("\"" + line + "\"");
+        }
+    }
+}
+
