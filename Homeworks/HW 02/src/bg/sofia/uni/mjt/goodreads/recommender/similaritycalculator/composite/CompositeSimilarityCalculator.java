d4b0230e8c35136d15c149760b018ca6dfffccbe
Aneliya21 <115350258+Aneliya21@users.noreply.github.com>
2024-12-27 17:44:27 +0200

    Create CompositeSimilarityCalculator.java

---
+++ Homeworks/HW 02/src/bg/sofia/uni/mjt/goodreads/recommender/similaritycalculator/composite/CompositeSimilarityCalculator.java
@@ -0,0 +1,34 @@
+package bg.sofia.uni.fmi.mjt.goodreads.recommender.similaritycalculator.composite;
+
+import bg.sofia.uni.fmi.mjt.goodreads.book.Book;
+import bg.sofia.uni.fmi.mjt.goodreads.recommender.similaritycalculator.SimilarityCalculator;
+
+import java.util.Map;
+
+public class CompositeSimilarityCalculator implements SimilarityCalculator {
+
+    private final Map<SimilarityCalculator, Double> similarityCalculatorMap;
+
+    public CompositeSimilarityCalculator(Map<SimilarityCalculator,
+        Double> similarityCalculatorMap) {
+        if (similarityCalculatorMap == null || similarityCalculatorMap.isEmpty()) {
+            throw new IllegalArgumentException("The similarity calculator map " +
+                "cannot be null or empty");
+        }
+        this.similarityCalculatorMap = similarityCalculatorMap;
+    }
+
+    @Override
+    public double calculateSimilarity(Book first, Book second) {
+        if (first == null || second == null) {
+            throw new IllegalArgumentException("Books cannot be null");
+        }
+        return similarityCalculatorMap.entrySet().stream()
+            .mapToDouble(entry -> {
+                SimilarityCalculator calculator = entry.getKey();
+                double weight = entry.getValue();
+                return calculator.calculateSimilarity(first, second) * weight;
+            })
+            .sum();
+    }
+}
